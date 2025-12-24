7b4e58fce84f69e7cf619b7c683344167ed1f071
xCyberpunk <peeplil6666@gmail.com>
2025-12-24 17:58:42 +0100

    latest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import SubHeroSection from "@/components/sections/sub-hero";
 import ImpactMarquee from "@/components/sections/impact-marquee";
 import MyProjects from "@/components/sections/projects";
 import ChampionCollection from "@/components/sections/champion-collection";
-import Partnerships from "@/components/sections/partnerships";
 import Footer from "@/components/sections/footer";
 import SplashScreen from "@/components/ui/splash-screen";
 import SmoothScroll from "@/components/ui/smooth-scroll";
@@ -26,7 +25,7 @@ export default function Home() {
         <TechExperience />
         <MyProjects />
         <ChampionCollection />
-        <Partnerships />
+       
         <Footer />
       </SmoothScroll>
     </main>
